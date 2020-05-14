b41b28774c78300c4fdae282006ab6d63112132b
Rishab Haridas <rishabharidas@users.noreply.github.com>
2020-05-14 17:03:48 +0530

    alpha build

---
+++ src/Database/DatabaseConnection.java
@@ -0,0 +1,68 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Database;
+
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author rbd
+ */
+public class DatabaseConnection {
+
+    String db = "vidattendence";
+    String username = "root";
+    String password = "root";
+    Connection con = null;
+    Statement stmt;
+    ResultSet rs = null;
+
+    public Connection dbconnection() {
+
+        try {
+            String url = "jdbc:mysql://localhost:3306/" + db ; 
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            con = DriverManager.getConnection(url, username, password);
+        } 
+        catch (Exception ex) {
+            Logger.getLogger(DatabaseConnection.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return con;
+    }
+
+    public ResultSet getResultSet(String query) {
+        try {
+            stmt = con.createStatement();
+            rs = stmt.executeQuery(query);
+        } catch (SQLException ex) {
+            Logger.getLogger(DatabaseConnection.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return rs;
+    }
+
+    public int getUpdate(String query) {
+        int i = 0;
+        try {
+            stmt = con.createStatement();
+            i = stmt.executeUpdate(query);
+        } catch (Exception e) {
+        }
+        return i;
+    }
+
+    public int executeQuery(String query) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public PreparedStatement prepareStatement(String query) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public Statement createStatement() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
